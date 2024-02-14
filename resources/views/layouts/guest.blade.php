647faaea03a74b33966155c5c545895ad41c833f
GerardoGR <gerardo_1322134023@uptecamac.edu.mx>
2024-02-14 01:07:53 -0600

    changes in view login

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -13,9 +13,69 @@
 
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
+        <style>
+            * {
+                margin: 0;
+                padding: 0;
+                box-sizing: border-box;
+            }
+            .header {
+                background-color: #13322B; /* Cambio de color */
+                color: white;
+                display: flex;
+                align-items: center;
+                justify-content: space-between;
+                height: 80px;
+                padding: 20px;
+            }
+            .footer {
+                background-color: #9D2449; /* Cambio de color */
+                color: white;
+                display: flex;
+                align-items: center;
+                justify-content: center;
+                height: 80px;
+                padding: 20px;
+            }
+            .content {
+                background-color: #FFFFFF; /* Cambio de color */
+                min-height: calc(100vh - 160px);
+                display: flex;
+                flex-direction: column;
+                align-items: center;
+                justify-content: center;
+            }
+            .login {
+                width: 300px;
+                border: 1px solid #ccc;
+                padding: 20px;
+            }
+            .login input {
+                width: 100%;
+                margin: 10px 0;
+                padding: 10px;
+                border: 1px solid #ccc;
+            }
+            .login button {
+                width: 100%;
+                margin: 10px 0;
+                padding: 10px;
+                border: 1px solid #621132; /* Cambio de color */
+                background-color: white;
+                color: #621132; /* Cambio de color */
+            }
+            .login a {
+                display: block;
+                text-align: center;
+                color: #006633;
+                text-decoration: none;
+            }
+        </style>
+
+
     </head>
     <body class="font-sans  antialiased">
-    <header class="header">}
+    <header class="header">
         <h1>Liconsa</h1>
         <h2>Gobierno de México</h2>
     </header>
@@ -26,9 +86,12 @@
                 </a>
             </div>
 
-            <div class="w-full sm:max-w-md mt-6 px-6 py-4  shadow-md overflow-hidden sm:rounded-lg">
+            <div class=" w-full sm:max-w-md mt-6 px-6 py-4  shadow-md overflow-hidden sm:rounded-lg">
                 {{ $slot }}
             </div>
         </div>
     </body>
+    <footer class="footer">
+        <p>LICONSA © 2024</p>
+    </footer>
 </html>
