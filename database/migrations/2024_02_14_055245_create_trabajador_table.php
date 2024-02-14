ced903acd14e3beab8c69be536672f07eea4d4cb
GerardoGR <gerardo_1322134023@uptecamac.edu.mx>
2024-02-14 00:28:29 -0600

    migrations

---
+++ database/migrations/2024_02_14_055245_create_trabajador_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('trabajador', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre',50);
+            $table->string('apellido_p',50);
+            $table->string('apellido_m',50);
+            $table->string('curp',18);
+            $table->string('rol',30);
+            $table->string('rfc',13);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('trabajador');
+    }
+};
---
+++ database/migrations/2024_02_14_055305_create_venta_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('venta', function (Blueprint $table) {
+            $table->id();
+            $table->string('folio',50);
+            $table->date('fecha');
+            $table->time('hora');
+            $table->integer('litros');
+            $table->float('Total');
+            $table->unsignedBigInteger('tutor_id'); // Cambiado a unsignedBigInteger
+            $table->unsignedBigInteger('trabajador_id');
+            $table->timestamps();
+
+            $table->foreign('tutor_id')->references('id')->on('tutor_legal');
+            $table->foreign('trabajador_id')->references('id')->on('trabajador');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('venta');
+    }
+};
