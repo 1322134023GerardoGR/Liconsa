8e7e0029909cf516f0c98df0bdb11182b16defce
GerardoGR <gerardo_1322134023@uptecamac.edu.mx>
2024-02-18 19:16:13 -0600

    Update guest.blade.php

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -80,13 +80,9 @@
         <h2>Gobierno de México</h2>
     </header>
         <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 ">
-            <div>
-                <a href="/">
-                    <x-application-logo class="w-20 h-20 fill-current " />
-                </a>
-            </div>
 
-            <div class=" w-full sm:max-w-md mt-6 px-6 py-4  shadow-md overflow-hidden sm:rounded-lg">
+
+            <div class=" w-full sm:max-w-md mt-6 px-4 py-4  shadow-md overflow-hidden sm:rounded-lg">
                 {{ $slot }}
             </div>
         </div>
