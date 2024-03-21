c69b7ba9a0fdf20a234a17da86dcff0ba05dfe1e
GerardoGR <gerardo_1322134023@uptecamac.edu.mx>
2024-03-21 03:01:04 -0600

    add seeder

--- database/migrations/2024_03_07_051951_create_ventas_table.php
+++ database/migrations/2024_03_07_051951_create_ventas_table.php
@@ -20,7 +20,7 @@ return new class extends Migration
             $table->float('total');
             $table->string('num_lecheria',10);
             $table->unsignedBigInteger('beneficiario_id');
-
+            $table->timestamps();
             $table->foreign('beneficiario_id')->references('id')->on('beneficiarios');
         });
     }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,10 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\beneficiario;
+use App\Models\trabajador;
+use App\Models\venta;
+use Faker\Factory as Faker;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -19,5 +23,65 @@ class DatabaseSeeder extends Seeder
              'email' => 'test@example.com',
              'password' => bcrypt('password'),
          ]);
+
+        $faker = Faker::create();
+
+        for ($i = 0; $i < 10; $i++) { // Genera 10 beneficiarios, puedes ajustar este número según tus necesidades
+            Beneficiario::create([
+                'nombre' => $faker->firstName,
+                'apellido_p' => $faker->lastName,
+                'apellido_m' => $faker->lastName,
+                'curp' => $faker->unique()->regexify('[A-Z]{4}[0-9]{6}[A-Z]{6}[0-9]{2}'),
+                'fecha_nac' => $faker->date,
+                'n_dependientes' => $faker->numberBetween(1, 10),
+                'direccion' => $faker->address,
+                'num_lecheria' => $faker->numerify('####-####'),
+                'folio_cb' => $faker->numberBetween(0, 999999999),
+                'Sancionado' => $faker->boolean(10) // 10% de los beneficiarios son sancionados
+            ]);
+        }
+
+        $faker = Faker::create();
+
+        $roles = ['vendedor', 'atencion_clientes', 'supervisor'];
+
+        for ($i = 0; $i < 10; $i++) { // Genera 10 trabajadores, puedes ajustar este número según tus necesidades
+            Trabajador::create([
+                'nombre' => $faker->firstName,
+                'apellido_p' => $faker->lastName,
+                'apellido_m' => $faker->lastName,
+                'curp' => $faker->unique()->regexify('[A-Z]{4}[0-9]{6}[A-Z]{6}[0-9]{2}'),
+                'rfc' => $faker->unique()->regexify('[A-Z]{4}[0-9]{6}[A-Z0-9]{3}'),
+                'rol' => $faker->randomElement($roles),
+            ]);
+        }
+
+        $faker = Faker::create();
+
+        // Obtener todos los beneficiarios
+        $beneficiarios = Beneficiario::all();
+
+        foreach ($beneficiarios as $beneficiario) {
+            // Generar ventas para cada beneficiario
+            for ($i = 0; $i < 2; $i++) { // Genera 5 ventas por beneficiario, ajusta según tus necesidades
+                $litrosVendidos = $faker->numberBetween(1, 100);
+                $numLecheria = $faker->numberBetween(1, 10);
+                $fecha = $faker->dateTimeThisYear();
+                $total = $litrosVendidos * 6.50;
+
+                // Crear la venta
+                $venta = new Venta();
+                $venta->beneficiario_id = $beneficiario->id;
+                $venta->litros_v = $litrosVendidos;
+                $venta->num_lecheria = $numLecheria;
+                $venta->folio = $faker->unique()->randomNumber(9);
+                $venta->fecha = $fecha->format('Y-m-d');
+                $venta->hora = $fecha->format('H:i:s');
+                $venta->total = $total;
+                $venta->save();
+            }
+        }
+
+
     }
 }
