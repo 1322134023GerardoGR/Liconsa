f1e05cf9289f6f34d1ea97b7045164085f6e8606
Marco Rodriguez <marco_1322134127@uptecamac.edu.mx>
2025-07-08 13:42:16 -0600

    feat: improve route management with throttling
    - Added middleware for rate limiting
    - Indented routes for better readability
    - Grouped route definitions under relevant middlewares
    - Consolidated route definitions for consistency
    - Removed redundant lines for cleaner code

--- routes/web.php
+++ routes/web.php
@@ -21,93 +21,87 @@ use App\Mail\NuevoBeneficiarioMail;
 |
 */
 
+Route::middleware(['throttle:20,1'])->group(function () {
 
-Route::get('/1', function () {
-    return view('welcome');
-});
+    Route::get('/1', function () {
+        return view('welcome');
+    });
 
-Route::get('/', function () {
-    return view('liconsa.index');
-})->name('index');
+    Route::get('/', function () {
+        return view('liconsa.index');
+    })->name('index');
 
 
-Route::get('/2', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+    Route::get('/2', function () {
+        return view('dashboard');
+    })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
+    Route::middleware('auth')->group(function () {
+        Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+        Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+        Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    });
 
-require __DIR__ . '/auth.php';
+    require __DIR__ . '/auth.php';
 
-Route::match(['get', 'post'],'/index', function () {
-    return view('liconsa.index');
-})->name('index');
+    Route::match(['get', 'post'], '/index', function () {
+        return view('liconsa.index');
+    })->name('index');
 
-Route::get('/add', function () {
-    return view('liconsa.agrBeneficiario');
-})->name('beneficiarios.nuevo');
+    Route::get('/add', function () {
+        return view('liconsa.agrBeneficiario');
+    })->name('beneficiarios.nuevo');
 
-Route::get('/see/{id}', function () {
-    return view('liconsa.seeBeneficiario');
-})->name('details.ben');
+    Route::get('/see/{id}', function () {
+        return view('liconsa.seeBeneficiario');
+    })->name('details.ben');
 
-Route::get('/edit/{id}', function () {
-    return view('liconsa.editBeneficiario');
-})->name('edit.ben');
+    Route::get('/edit/{id}', function () {
+        return view('liconsa.editBeneficiario');
+    })->name('edit.ben');
 
-Route::get('/edit/{id}', function () {
-    return view('liconsa.editUser');
-})->name('edit.user');
+    Route::get('/edit/{id}', function () {
+        return view('liconsa.editUser');
+    })->name('edit.user');
 
-Route::get('/list', function () {
-    return view('liconsa.listBene');
-})->name('list.ben');
+    Route::get('/list', function () {
+        return view('liconsa.listBene');
+    })->name('list.ben');
 
-Route::get('/addU', function () {
-    return view('liconsa.addUser');
-})->name('user.nuevo');
+    Route::get('/addU', function () {
+        return view('liconsa.addUser');
+    })->name('user.nuevo');
 
-Route::get('/addS', function () {
-    return view('liconsa.addSell');
-})->name('add.sell');
+    Route::get('/addS', function () {
+        return view('liconsa.addSell');
+    })->name('add.sell');
 
-Route::get('/card-pdf/{id}', [PdfController::class, 'generatePDF'])->name('pdf.generate');
+    Route::get('/card-pdf/{id}', [PdfController::class, 'generatePDF'])->name('pdf.generate');
 
-Route::post('/beneficiarios/store', [BeneficiarioController::class, 'store'])->name('beneficiarios.store');
-Route::match(['get', 'post'], '/beneficiarios/update/{id}', [BeneficiarioController::class, 'update'])->name('beneficiarios.update');
-Route::match(['get', 'post'], '/beneficiarios/editar/{id}', [BeneficiarioController::class, 'edit'])->name('beneficiarios.edit');
-Route::match(['get', 'post'], '/beneficiarios/detalles/{id}', [BeneficiarioController::class, 'show'])->name('beneficiarios.show');
-Route::match(['get', 'post'], '/beneficiarios/eliminar/{id}', [BeneficiarioController::class, 'destroy'])->name('beneficiarios.destroy');
-Route::match(['get', 'post'], '/beneficiarios/imagen', [BeneficiarioController::class, 'savePhoto'])->name('beneficiarios.imagen');
-Route::match(['get', 'post', 'delete'], '/beneficiarios/lista', [BeneficiarioController::class, 'index'])->name('beneficiarios.list');
-Route::match(['get', 'post'], '/beneficiarios/buscar/', [BeneficiarioController::class, 'buscar'])->name('beneficiarios.buscar');
+    Route::post('/beneficiarios/store', [BeneficiarioController::class, 'store'])->name('beneficiarios.store');
+    Route::match(['get', 'post'], '/beneficiarios/update/{id}', [BeneficiarioController::class, 'update'])->name('beneficiarios.update');
+    Route::match(['get', 'post'], '/beneficiarios/editar/{id}', [BeneficiarioController::class, 'edit'])->name('beneficiarios.edit');
+    Route::match(['get', 'post'], '/beneficiarios/detalles/{id}', [BeneficiarioController::class, 'show'])->name('beneficiarios.show');
+    Route::match(['get', 'post'], '/beneficiarios/eliminar/{id}', [BeneficiarioController::class, 'destroy'])->name('beneficiarios.destroy');
+    Route::match(['get', 'post'], '/beneficiarios/imagen', [BeneficiarioController::class, 'savePhoto'])->name('beneficiarios.imagen');
+    Route::match(['get', 'post', 'delete'], '/beneficiarios/lista', [BeneficiarioController::class, 'index'])->name('beneficiarios.list');
+    Route::match(['get', 'post'], '/beneficiarios/buscar/', [BeneficiarioController::class, 'buscar'])->name('beneficiarios.buscar');
 
 //Route::get('/beneficiarios/buscar', BeneficiarioController::class, 'buscar')->name('beneficiarios.buscar');
 
 
-Route::post('/trabajadores/store', [TrabajadorController::class, 'store'])->name('trabajadores.store');
-Route::match(['get', 'post'], '/trabajadores/update/{id}', [TrabajadorController::class, 'update'])->name('trabajadores.update');
-Route::match(['get', 'post'], '/trabajadores/editar/{id}', [TrabajadorController::class, 'edit'])->name('trabajadores.edit');
-Route::match(['get', 'post'], '/trabajadores/detalles/{id}', [TrabajadorController::class, 'show'])->name('trabajadores.show');
-Route::match(['get', 'post'], '/trabajadores/eliminar/{id}', [TrabajadorController::class, 'destroy'])->name('trabajadores.destroy');
-Route::match(['get', 'post', 'delete'], '/trabajadores/lista', [TrabajadorController::class, 'index'])->name('trabajadores.list');
+    Route::post('/trabajadores/store', [TrabajadorController::class, 'store'])->name('trabajadores.store');
+    Route::match(['get', 'post'], '/trabajadores/update/{id}', [TrabajadorController::class, 'update'])->name('trabajadores.update');
+    Route::match(['get', 'post'], '/trabajadores/editar/{id}', [TrabajadorController::class, 'edit'])->name('trabajadores.edit');
+    Route::match(['get', 'post'], '/trabajadores/detalles/{id}', [TrabajadorController::class, 'show'])->name('trabajadores.show');
+    Route::match(['get', 'post'], '/trabajadores/eliminar/{id}', [TrabajadorController::class, 'destroy'])->name('trabajadores.destroy');
+    Route::match(['get', 'post', 'delete'], '/trabajadores/lista', [TrabajadorController::class, 'index'])->name('trabajadores.list');
 
 
-Route::post('/ventas/store', [VentaController::class, 'store'])->name('ventas.store');
-Route::match(['get', 'post'], '/ventas/update/{id}', [VentaController::class, 'update'])->name('ventas.update');
-Route::match(['get', 'post'], '/ventas/editar/{id}', [VentaController::class, 'edit'])->name('ventas.edit');
-Route::match(['get', 'post'], '/ventas/detalles/{id}', [VentaController::class, 'show'])->name('ventas.show');
-Route::match(['get', 'post'], '/ventas/eliminar/{id}', [VentaController::class, 'destroy'])->name('ventas.destroy');
-Route::match(['get', 'post', 'delete'], '/ventas/lista', [VentaController::class, 'index'])->name('ventas.list');
-
-
-
-
-
-
-
-
+    Route::post('/ventas/store', [VentaController::class, 'store'])->name('ventas.store');
+    Route::match(['get', 'post'], '/ventas/update/{id}', [VentaController::class, 'update'])->name('ventas.update');
+    Route::match(['get', 'post'], '/ventas/editar/{id}', [VentaController::class, 'edit'])->name('ventas.edit');
+    Route::match(['get', 'post'], '/ventas/detalles/{id}', [VentaController::class, 'show'])->name('ventas.show');
+    Route::match(['get', 'post'], '/ventas/eliminar/{id}', [VentaController::class, 'destroy'])->name('ventas.destroy');
+    Route::match(['get', 'post', 'delete'], '/ventas/lista', [VentaController::class, 'index'])->name('ventas.list');
+}
